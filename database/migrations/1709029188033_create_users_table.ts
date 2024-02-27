17365bba9138083ba5c7eb0c1c62352147e192a6
PierreRogier <pierre.rogier3@gmail.com>
2024-02-27 11:31:33 +0100

    update user model

--- database/migrations/1709029188033_create_users_table.ts
+++ database/migrations/1709029188033_create_users_table.ts
@@ -6,9 +6,10 @@ export default class extends BaseSchema {
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').notNullable()
-      table.string('full_name').nullable()
       table.string('email', 254).notNullable().unique()
-      table.string('password').notNullable()
+      table.string('password', 200).notNullable()
+      table.string('firstname', 100).notNullable()
+      table.string('lastname', 100).notNullable()
 
       table.timestamp('created_at').notNullable()
       table.timestamp('updated_at').nullable()
